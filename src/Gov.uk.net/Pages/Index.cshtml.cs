6fa60edb95a7900f29f982bc0a4a254396e7c7fa
David Joseph <scrambledheads@gmail.com>
2021-02-02 19:06:59 +0000

    Accordion implemented

--- src/Gov.uk.net/Pages/Index.cshtml.cs
+++ src/Gov.uk.net/Pages/Index.cshtml.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Gov.Uk.Net.Library.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace Gov.uk.net.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public List<AccordionItem> Items;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +19,13 @@ namespace Gov.uk.net.Pages
 
         public void OnGet()
         {
-
+            Items = new List<AccordionItem>()
+            {
+                new AccordionItem("Writing well for the web", string.Empty, new HtmlString("<p class=\"govuk-body\">This is the content for Writing well for the web.</p>"), false),
+                new AccordionItem("Writing well for specialists", string.Empty, new HtmlString("<p class=\"govuk-body\">This is the content for Writing well for specialists.</p>"), false),
+                new AccordionItem("Know your audience", string.Empty, new HtmlString("<p class=\"govuk-body\">This is the content for Know your audience.</p>"), false),
+                new AccordionItem("How people read", string.Empty, new HtmlString("<p class=\"govuk-body\">This is the content for How people read.</p>"), false)
+            };
         }
     }
 }
